d18b93020916891b9d23c3286e5202eb4a45fb61
Luca Sartore <drwolf85@gmail.com>
2025-01-25 23:58:04 -0500

    Update `downuz.py` to newer packages

--- downuz.py
+++ downuz.py
@@ -6,21 +6,20 @@ Script to download GDELT data-sets and unzip them
 """
 
 import os
-import urllib
+import urllib.request as download
 import datetime
 import zipfile
 
 if not os.path.exists("./data/"):
     os.makedirs("./data/")
 
-datafile = urllib.URLopener()
-
 yesterday = datetime.datetime.now() - datetime.timedelta(days = 1)
 sufX = [".export.CSV.zip", ".gkg.csv.zip", ".gkgcounts.csv.zip"]
 filenames = [yesterday.strftime("%Y%m%d" + i) for i in sufX]
-datafile.retrieve("http://data.gdeltproject.org/events/" + filenames[0], filenames[0])
-datafile.retrieve("http://data.gdeltproject.org/gkg/" + filenames[1], filenames[1])
-datafile.retrieve("http://data.gdeltproject.org/gkg/" + filenames[2], filenames[2])
+
+download.urlretrieve("http://data.gdeltproject.org/events/" + filenames[0], filenames[0])
+download.urlretrieve("http://data.gdeltproject.org/gkg/" + filenames[1], filenames[1])
+download.urlretrieve("http://data.gdeltproject.org/gkg/" + filenames[2], filenames[2])
 
 for myfile in filenames:
     with zipfile.ZipFile(myfile, "r") as z:
